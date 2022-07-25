77a6a340408f2c56e10d8ffd3d88bedb3202d160
= <=>
2022-07-26 00:14:49 +0300

    ayarlar sayfası düzenlendi

--- ayarlar.php
+++ ayarlar.php
@@ -1,5 +1,36 @@
-<?php include('header.php');?>
+<?php include('header.php'); ?>
+
+<div class="container-fluid">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="text-primary font-weight-bold">Ayarlar</h3>
+        </div>
+
+        <div class="card-body">
+            <form action="ayarlar_submit" method="get">
+                <div class="form-row">
+                    <div class="col-md-6">
+                        <label>Sitenizin başlığını giriniz</label>
+                        <input class="form-control" type="text" name="" placeholder="Sitenizin başlığını giriniz">
+                    </div>
+                </div>
+                <div class="form-row my-2">
+                    <div class="col-md-6">
+                        <label>Sitenizin açıklamasını giriniz</label>
+                        <input class="form-control" type="text" name="" placeholder="Sitenizin açıklamasını giriniz">
+                    </div>
+                </div>
+                <div class="form-row my-2">
+                    <div class="col-md-6">
+                        <label>Site sahibi</label>
+                        <input class="form-control" type="text" name="" placeholder="Sitenizin  Sahibi">
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary mt-2">Kaydet</button>
+            </form>
+        </div>
+    </div>
+</div>
 
 
-
-<?php include('footer.php');?>
+<?php include('footer.php'); ?>
